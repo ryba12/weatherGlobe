b0ba9e1bee3aca945c6a8a42c46f905bbc637c03
jablonskik <k.jablonski@telejur.net>
2021-01-21 15:13:29 +0100

    fix previous button label

--- weatherGlobe/src/app/models/texts.ts
+++ weatherGlobe/src/app/models/texts.ts
@@ -1,7 +1,7 @@
 export enum Texts {
     ADD_LOCATION = "Dodaj lokalizację",
     YOUR_LOCATION = "Twoja lokalizacja",
-    BACK = "Twoja lokalizacja",
+    BACK = "Wcześniej",
     NEXT = "Dalej",
     SEARCH_PLACE = "Znajdź miejscowość",
     MAIN_PANEL = "Panel główny"
